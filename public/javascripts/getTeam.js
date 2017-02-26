fd618744738eef8dc1592707a38f01f71d88b33c
Logan C H <losesmc@gmail.com>
2017-02-25 19:19:11 -0700

    Semi-working state

---
+++ public/javascripts/getTeam.js
@@ -0,0 +1,9 @@
+var common = document.getElementById('common');
+var different = document.getElementById('different');
+
+common.onclick = function() {
+    alert("Common interest getting start!");
+};
+different.onclick = function() {
+    alert("Different interest getting start!");
+};
