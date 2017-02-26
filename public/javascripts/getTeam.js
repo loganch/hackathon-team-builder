2e257ad8c7e61085a098bc6f6ef5d540eafe036f
Logan C H <losesmc@gmail.com>
2017-02-25 21:48:44 -0700

    Implement server-side for Sheets data

--- public/javascripts/getTeam.js
+++ public/javascripts/getTeam.js
@@ -1,102 +1,32 @@
 var common = document.getElementById('common');
 var different = document.getElementById('different');
 
-var GoogleSpreadsheet = require('google-spreadsheet');
-var Async = require('async');
-var doc = new GoogleSpreadsheet('1VaSkPxcKhNXHhFoMMs5OGyXjB-gZQZamTL9ue6YRKiw');
-var sheet;
-
-async.series([
-  function setAuth(step) {
-    // see notes below for authentication instructions!
-    var creds = require('./google-generated-creds.json');
-    // OR, if you cannot save the file locally (like on heroku)
-    var creds_json = {
-      client_email: 'yourserviceaccountemailhere@google.com',
-      private_key: 'your long private key stuff here'
-    }
- 
-    doc.useServiceAccountAuth(creds, step);
-  },
-  function getInfoAndWorksheets(step) {
-    doc.getInfo(function(err, info) {
-      console.log('Loaded doc: '+info.title+' by '+info.author.email);
-      sheet = info.worksheets[0];
-      console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
-      step();
-    });
-  },
-  function workingWithRows(step) {
-    // google provides some query options
-    sheet.getRows({
-      offset: 1,
-      limit: 20,
-      orderby: 'col2'
-    }, function( err, rows ){
-      console.log('Read '+rows.length+' rows');
-
-      // the row is an object with keys set by the column headers
-      rows[0].colname = 'new val';
-      rows[0].save(); // this is async
-
-      // deleting a row
-      rows[0].del();  // this is async
-
-      step();
-    });
-  },
-  function workingWithCells(step) {
-    sheet.getCells({
-      'min-row': 1,
-      'max-row': 5,
-      'return-empty': true
-    }, function(err, cells) {
-      var cell = cells[0];
-      console.log('Cell R'+cell.row+'C'+cell.col+' = '+cells.value);
-
-      // cells have a value, numericValue, and formula
-      cell.value == '1'
-      cell.numericValue == 1;
-      cell.formula == '=ROW()';
-
-      // updating `value` is "smart" and generally handles things for you
-      cell.value = 123;
-      cell.value = '=A1+B2'
-      cell.save(); //async
-
-      // bulk updates make it easy to update many cells at once
-      cells[0].value = 1;
-      cells[1].value = 2;
-      cells[2].formula = '=A1+B1';
-      sheet.bulkUpdateCells(cells); //async
-
-      step();
-    });
-  },
-  function managingSheets(step) {
-    doc.addWorksheet({
-      title: 'my new sheet'
-    }, function(err, sheet) {
-
-      // change a sheet's title
-      sheet.setTitle('new title'); //async
-
-      //resize a sheet
-      sheet.resize({rowCount: 50, colCount: 20}); //async
-
-      sheet.setHeaderRow(['name', 'age', 'phone']); //async
-
-      // removing a worksheet
-      sheet.del(); //async
-
-      step();
-    });
-  }
-]);
 
 common.onclick = function() {
-    alert("Common interest getting start!");
+    var client = new HttpClient();
+    client.get('/common', function(res) {
+        var rows = JSON.parse(res);
+        for (var i in rows) {
+            console.log(rows[i].firstname);
+        }
+    });
 };
 different.onclick = function() {
-    alert("Different interest getting start!");
+    var client = new HttpClient();
+    client.get('/different', function(res) {
+        alert('Responded with ' + res);
+    });
 };
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() {
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open("GET", aUrl, true);
+        anHttpRequest.send(null);
+    }
+}
